da3655ab17a5007223da44f2649509936948e368
Dali47 <meftah.dali@gmail.com>
2022-05-16 15:57:43 +0100

    Adding the data send and the raspberry code

---
+++ DoorOpner.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python3
+import RPi.GPIO as GPIO
+import time
+import paho.mqtt.client as paho
+from paho import mqtt
+
+# setting callbacks for different events to see if it works, print the message etc.
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("CONNACK received with code %s." % rc)
+
+# with this callback you can see if your publish was successful
+def on_publish(client, userdata, mid, properties=None):
+    print("mid: " + str(mid))
+
+# print which topic was subscribed to
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+# print message, useful for checking if it was successful
+def on_message(client, userdata, msg):
+    print(msg.topic + ""+" Une personne est detecter" +""+ " La porte va etre ouverte" + " " +" Le nombre totale des perssone:" + str(msg.payload))
+    in1 = 17
+    in2 = 18
+    in3 = 27
+    in4 = 22
+     
+    # careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
+    step_sleep = 0.002
+     
+    step_count = 1024 # 5.625*(1/64) per step, 4096 steps is 360°
+     
+    direction = False # True for clockwise, False for counter-clockwise
+     
+    # defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
+    step_sequence = [[1,0,0,1],
+                     [1,0,0,0],
+                     [1,1,0,0],
+                     [0,1,0,0],
+                     [0,1,1,0],
+                     [0,0,1,0],
+                     [0,0,1,1],
+                     [0,0,0,1]]
+     
+    # setting up
+    GPIO.setmode( GPIO.BCM )
+    GPIO.setup( in1, GPIO.OUT )
+    GPIO.setup( in2, GPIO.OUT )
+    GPIO.setup( in3, GPIO.OUT )
+    GPIO.setup( in4, GPIO.OUT )
+     
+    # initializing
+    GPIO.output( in1, GPIO.LOW )
+    GPIO.output( in2, GPIO.LOW )
+    GPIO.output( in3, GPIO.LOW )
+    GPIO.output( in4, GPIO.LOW )
+     
+     
+    motor_pins = [in1,in2,in3,in4]
+    motor_step_counter = 0 ;
+     
+     
+    def cleanup():
+        GPIO.output( in1, GPIO.LOW )
+        GPIO.output( in2, GPIO.LOW )
+        GPIO.output( in3, GPIO.LOW )
+        GPIO.output( in4, GPIO.LOW )
+        GPIO.cleanup()
+     
+     
+    # the meat
+    try:
+        i = 0
+        for i in range(step_count):
+            for pin in range(0, len(motor_pins)):
+                GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
+            if direction==True:
+                motor_step_counter = (motor_step_counter - 1) % 8
+            elif direction==False:
+                motor_step_counter = (motor_step_counter + 1) % 8
+            else: # defensive programming
+                print( "uh oh... direction should *always* be either True or False" )
+                cleanup()
+                exit( 1 )
+            time.sleep( step_sleep )
+     
+    except KeyboardInterrupt:
+        cleanup()
+        exit( 1 )
+     
+    cleanup()
+
+    time.sleep(5)
+
+    direction = True # True for clockwise, False for counter-clockwise
+     
+    # defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
+    step_sequence = [[1,0,0,1],
+                     [1,0,0,0],
+                     [1,1,0,0],
+                     [0,1,0,0],
+                     [0,1,1,0],
+                     [0,0,1,0],
+                     [0,0,1,1],
+                     [0,0,0,1]]
+     
+    # setting up
+    GPIO.setmode( GPIO.BCM )
+    GPIO.setup( in1, GPIO.OUT )
+    GPIO.setup( in2, GPIO.OUT )
+    GPIO.setup( in3, GPIO.OUT )
+    GPIO.setup( in4, GPIO.OUT )
+     
+    # initializing
+    GPIO.output( in1, GPIO.LOW )
+    GPIO.output( in2, GPIO.LOW )
+    GPIO.output( in3, GPIO.LOW )
+    GPIO.output( in4, GPIO.LOW )
+     
+     
+    motor_pins = [in1,in2,in3,in4]
+    motor_step_counter = 0 ;
+     
+     
+    def cleanup():
+        GPIO.output( in1, GPIO.LOW )
+        GPIO.output( in2, GPIO.LOW )
+        GPIO.output( in3, GPIO.LOW )
+        GPIO.output( in4, GPIO.LOW )
+        GPIO.cleanup()
+     
+     
+    # the meat
+    try:
+        i = 0
+        for i in range(step_count):
+            for pin in range(0, len(motor_pins)):
+                GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
+            if direction==True:
+                motor_step_counter = (motor_step_counter - 1) % 8
+            elif direction==False:
+                motor_step_counter = (motor_step_counter + 1) % 8
+            else: # defensive programming
+                print( "uh oh... direction should *always* be either True or False" )
+                cleanup()
+                exit( 1 )
+            time.sleep( step_sleep )
+     
+    except KeyboardInterrupt:
+        cleanup()
+    
+    
+    
+    
+
+# using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
+# userdata is user defined data of any type, updated by user_data_set()
+# client_id is the given name of the client
+client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+client.on_connect = on_connect
+
+# enable TLS for secure connection
+client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+# set username and password
+client.username_pw_set("Raspberry", "Raspberry123")
+# connect to HiveMQ Cloud on port 8883 (default for MQTT)
+client.connect("d62d6f41f62b4c0698d334a94fc4c32b.s1.eu.hivemq.cloud", 8883)
+
+# setting callbacks, use separate functions like above for better visibility
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+client.on_publish = on_publish
+
+# subscribe to all topics of encyclopedia by using the wildcard "#"
+client.subscribe("open/#", qos=1)
+
+# a single publish, this can also be done in loops, etc.
+#client.publish("mask/count", payload="hot", qos=1)
+
+# loop_forever for simplicity, here you need to stop the loop manually
+# you can also use loop_start and loop_stop
+client.loop_forever()
--- Scanner.py
+++ Scanner.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 import pyzbar.pyzbar as pyzbar
 from datetime import datetime
-
+import time
 import paho.mqtt.client as paho
 from paho import mqtt
 
@@ -13,6 +13,9 @@ font = cv2.FONT_HERSHEY_PLAIN
 cap = cv2.VideoCapture(0)
 
 currentDate = datetime.date(datetime.now())
+
+
+    
 
 while True:
     _, frame = cap.read()
@@ -26,8 +29,49 @@ while True:
             
             qrDate = datetime.strptime(qrDateString, '%Y-%m-%d').date()
         
-            if(currentDate > qrDate) :        
-                playsound('welcomme.mp3') 
+            if(currentDate > qrDate) :     
+                
+                     #----------------- Code de connection & envoie-----------------------
+                    def on_connect(client, userdata, flags, rc, properties=None):
+                        print("CONNACK received with code %s." % rc)
+                        
+                        # with this callback you can see if your publish was successful
+                    def on_publish(client, userdata, mid, properties=None):
+                            print("mid: " + str(mid))
+                        
+                        # print which topic was subscribed to
+                    def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+                            print("Subscribed: " + str(mid) + " " + str(granted_qos))
+                        
+                        # print message, useful for checking if it was successful
+                    def on_message(client, userdata, msg):
+                        print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+                        
+                        # using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
+                        # userdata is user defined data of any type, updated by user_data_set()
+                        # client_id is the given name of the client
+                    client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+                    client.on_connect = on_connect
+                        
+                        # enable TLS for secure connection
+                    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+                        # set username and password
+                    client.username_pw_set("pcFace", "Dali19974")
+                        # connect to HiveMQ Cloud on port 8883 (default for MQTT)
+                    client.connect("d62d6f41f62b4c0698d334a94fc4c32b.s1.eu.hivemq.cloud", 8883)
+                        
+                        # setting callbacks, use separate functions like above for better visibility
+                    client.on_subscribe = on_subscribe
+                    client.on_message = on_message
+                    client.on_publish = on_publish
+                        
+                        # subscribe to all topics of encyclopedia by using the wildcard "#"
+                    client.subscribe("open/#", qos=1)
+
+                    client.publish("open/door", payload=1, qos=1)
+                    #----------------------------------------
+
+                    playsound('welcomme.mp3') 
             else:
                 playsound('Expire.mp3') 
         except ValueError:
